4b9920cbdd7f77bb945adee38701bc2fcb274ab2
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-01-31 17:23:26 +0530

    c

--- multiples-of-3-or-5.py
+++ multiples-of-3-or-5.py
@@ -1,14 +1,14 @@
-# this way is O(1000)
-total_sum = 0
-for i in range(1, 1000):
-    if (i % 3 == 0 or i % 5 == 0):
-        total_sum = total_sum + i
-print(total_sum)
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
 
+def lcm(a, b):
+    return a * b // gcd(a, b)
 
-# this is O(1) using math (AP)
-val1 = 999//3 * (3 + 999) // 2
-val2 = 999//5 * (5 + 995) // 2
-val3 = 999//15 * (30 + (999//15-1) * 15) // 2
+val = 1
 
-print(val1 + val2 - val3)
+for i in range(2, 21):
+    val = lcm(val, i)
+print(val)
